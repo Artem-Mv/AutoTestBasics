9e92801a07ac2186646d0573daeaf148fec9dfa9
Artem-Mv <Stimul113@gmail.com>
2022-04-20 21:22:47 +0700

    fix test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,19 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void shouldReturnHundred() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        assertEquals(cashbackHackService.remain(900), 100);
+    }
+    @Test
+    public void shouldReturnZero() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        assertEquals(cashbackHackService.remain(1000), 0);
+    }
 }
